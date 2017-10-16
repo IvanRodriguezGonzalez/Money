65617df20668d27633da9b05a71cfe4cb9487a66
Ivan <ivan.rodriguez.gonzalez@gmail.com>
2017-10-16 12:09:27 +0200

    ok

--- src/tddMoney/MoneyTest.java
+++ src/tddMoney/MoneyTest.java
@@ -88,7 +88,7 @@ public class MoneyTest extends TestCase {
    }
 
    public void testSumTimes(){
-       Expression fiveBucks = Money.dollar(35);
+       Expression fiveBucks = Money.dollar(5);
        Expression tenFrancs = Money.franc(10);
        Bank bank = new Bank();
        bank.addRate("CHF","USD",2);
